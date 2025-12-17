af8be66a7403fec095e315f9a92034cd7653557d
BoonyakornTanrattankorn <boonyakorn.tan@gmail.com>
2025-12-17 10:52:42 +0700

    feat: implement density compute shader pipeline

---
+++ density_compute_shader.glsl
@@ -0,0 +1,50 @@
+#[compute]
+#version 450
+
+// Invocations in the (x, y, z) dimension
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+
+// A binding to the buffer we create in our script
+layout(set = 0, binding = 0, std430) restrict buffer PositionBuffer {
+    vec2 position[];
+}
+position;
+
+layout(set = 0, binding = 1, std430) restrict buffer DensityBuffer {
+    float density[];
+}
+density;
+
+layout(set = 0, binding = 2, std430) restrict buffer ParamBuffer {
+    float mass;
+    float particle_num;
+    float smoothing_length;
+}
+param;
+
+float pow(float base, int exp){
+    float result = 1.0;
+    for(int i = 0; i < exp; ++i){
+        result *= base;
+    }
+    return result;
+}
+
+#define PI 3.14159265359
+const float alpha = 15 / (7*PI*param.smoothing_length*param.smoothing_length); // kernel normalization factor for 2d
+
+// smoothing kernel
+float W(float r):
+	var q = r / param.smoothing_length
+	if q >= 2:
+		return 0
+	else:
+		return alpha * ((1.0/6.0) * pow((2.0-q), 3));
+		
+// The code we want to execute in each invocation
+void main() {
+    int particle_index = int(gl_GlobalInvocationID.x);
+    for(int idx = 0; idx < param.particle_num; ++idx){
+        float dst = 
+    }
+}
--- main.gd
+++ main.gd
@@ -2,8 +2,17 @@ extends Node2D
 
 var rng = RandomNumberGenerator.new()
 
-var particle_array : Array[Particle] = []
-var particle_num = 0
+var rd := RenderingServer.create_local_rendering_device()
+var density_compute_shader_file := load("res://density_compute_shader.glsl")
+var density_compute_shader_spirv: RDShaderSPIRV = density_compute_shader_file.get_spirv()
+var density_compute_shader := rd.shader_create_from_spirv(density_compute_shader_spirv)
+var density_compute_pipeline := rd.compute_pipeline_create(density_compute_shader)
+
+var particle_position: PackedVector2Array = []
+var particle_velocity: PackedVector2Array = []
+
+var particle_array: Array[Particle] = []
+var particle_num = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,7 +35,7 @@ func _input(event: InputEvent):
 func _update_physics(delta: float) -> void:
 	# Calculate particles density
 	for particle_index in range(particle_array.size()):
-		particle_array[particle_index].density = _calculate_density(particle_array[particle_index]._position)
+		particle_array[particle_index].density = _calculate_density_cpu(particle_array[particle_index]._position)
 	# Calculate pressure force
 	for particle_index in range(particle_array.size()):
 		var pressure_force = _calculate_pressure_force(particle_index)
@@ -42,7 +51,7 @@ func _update_physics(delta: float) -> void:
 func _process(delta: float) -> void:
 	_update_physics(delta)
 	
-func _calculate_density(_position: Vector2) -> float:
+func _calculate_density_cpu(_position: Vector2) -> float:
 	var density = 0
 	for idx in range(particle_array.size()):
 		var dst = (particle_array[idx]._position - _position).length()
@@ -51,11 +60,66 @@ func _calculate_density(_position: Vector2) -> float:
 	Global.max_density = max(Global.max_density, density)
 	return density
 
+func _calculate_density_gpu() -> PackedFloat32Array:
+	# Position buffer (binding = 0)
+	var position_bytes := particle_position.to_byte_array()
+	var position_buffer := rd.storage_buffer_create(position_bytes.size(), position_bytes)
+
+	var position_uniform := RDUniform.new()
+	position_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	position_uniform.binding = 0
+	position_uniform.add_id(position_buffer)
+
+	# Density buffer (binding = 1)
+	var density_input := PackedFloat32Array()
+	density_input.resize(particle_num)
+	var density_bytes := density_input.to_byte_array()
+	var density_buffer := rd.storage_buffer_create(density_bytes.size(), density_bytes)
+
+	var density_uniform := RDUniform.new()
+	density_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	density_uniform.binding = 1
+	density_uniform.add_id(density_buffer)
+
+	# Param buffer (binding = 2)
+	var param := PackedFloat32Array([particle_num, Global.smoothing_radius, Global.particle_mass])
+	var param_bytes := param.to_byte_array()
+	var param_buffer := rd.storage_buffer_create(param_bytes.size(), param_bytes)
+
+	var param_uniform := RDUniform.new()
+	param_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	param_uniform.binding = 2
+	param_uniform.add_id(param_buffer)
+
+	# uniform set for set = 0
+	var uniform_set := rd.uniform_set_create(
+		[density_uniform, position_uniform, param_uniform],
+		density_compute_shader,
+		0
+	)
+
+	# dispatch compute shader
+	var compute_list := rd.compute_list_begin()
+	rd.compute_list_bind_compute_pipeline(compute_list, density_compute_pipeline)
+	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
+	rd.compute_list_dispatch(compute_list, particle_num, 1, 1)
+	rd.compute_list_end()
+
+	rd.submit()
+	# we can actually do other CPU tasks here while GPU is working
+	rd.sync ()
+
+	# read back density buffer
+	var density_output_bytes := rd.buffer_get_data(density_buffer)
+	var density_output := density_output_bytes.to_float32_array()
+	return density_output
+
+
 func _calculate_density_gradient(_position: Vector2) -> Vector2:
 	const delta = 1e-6
-	var origin = _calculate_density(_position)
-	var dx = _calculate_density(_position + Vector2.RIGHT * delta) - origin
-	var dy = _calculate_density(_position + Vector2.UP * delta) - origin
+	var origin = _calculate_density_cpu(_position)
+	var dx = _calculate_density_cpu(_position + Vector2.RIGHT * delta) - origin
+	var dy = _calculate_density_cpu(_position + Vector2.UP * delta) - origin
 	var dir = Vector2(-dx, dy)
 	return Vector2.ZERO if dir == Vector2.ZERO else dir / dir.length()
 	
@@ -70,7 +134,7 @@ func _calculate_pressure_force(particle_index: int) -> Vector2:
 		var dst = (particle_array[idx]._position - particle_array[particle_index]._position).length()
 		var other_pressure = _convert_density_to_pressure(particle_array[idx].density)
 		
-		var dir = Vector2(rng.randf(), rng.randf()) if dst == 0  \
+		var dir = Vector2(rng.randf(), rng.randf()) if dst == 0 \
 		else (particle_array[idx]._position - particle_array[particle_index]._position) / dst
 		var grad = SmoothingKernel.Wgrad(dst)
 		var shared_pressure = (self_pressure + other_pressure) / 2.0
